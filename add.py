920b3eb5366e19408565b1f1dd72f20fbce41c56
gkdevops1219 <girishkumarbhupala1219@gmail.com>
2025-06-16 02:11:14 +0530

    Update add.py
    added new varieties of fruits with prices

--- add.py
+++ add.py
@@ -3,3 +3,9 @@ apple_kg= 40
 pineapple_kg= 50
 total_bill= orange_kg+apple_kg+pineapple_kg
 print('total amount of friuts:',total_bill )
+
+pomogranate_kg= 80
+waterapple_kg= 60
+kiwi_kg= 20
+total_bill= pomogranate_kg+waterapple_kg+kiwi_kg
+print('total amount of friuts:',total_bill )
