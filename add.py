cb683b336d9374d0521c37a9d7d5b61caaf75d4c
gkdevops1219 <girishkumarbhupala1219@gmail.com>
2025-06-16 02:16:10 +0530

    added sub instead of add

--- add.py
+++ add.py
@@ -7,5 +7,5 @@ print('total amount of friuts:',total_bill )
 pomogranate_kg= 80
 waterapple_kg= 60
 kiwi_kg= 20
-total_bill= pomogranate_kg+waterapple_kg+kiwi_kg
-print('total amount of friuts:',total_bill )
+total_bill= pomogranate_kg-waterapple_kg-kiwi_kg
+print('total amount of friuts:' ,total_bill )
